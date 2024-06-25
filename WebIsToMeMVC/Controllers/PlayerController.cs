8354ba089c733436fbfd189f718ba0812e6e89d7
220337 <220337@LTI4102>
2024-06-25 16:20:54 -0300

    Adicionar arquivos de projeto.

---
+++ WebIsToMeMVC/Controllers/PlayerController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebIsToMeMVC.Models;
+
+namespace WebIsToMeMVC.Controllers
+{
+    public class PlayerController : Controller
+    {
+        private LP2Entities1 db = new LP2Entities1();
+
+        // GET: Player
+        public ActionResult Index()
+        {
+            return View(db.Player.ToList());
+        }
+
+        // GET: Player/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Player player = db.Player.Find(id);
+            if (player == null)
+            {
+                return HttpNotFound();
+            }
+            return View(player);
+        }
+
+        // GET: Player/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Player/Create
+        // Para proteger-se contra ataques de excesso de postagem, ative as propriedades específicas às quais deseja se associar. 
+        // Para obter mais detalhes, confira https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Nome,Nivel,Sexo")] Player player)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Player.Add(player);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(player);
+        }
+
+        // GET: Player/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Player player = db.Player.Find(id);
+            if (player == null)
+            {
+                return HttpNotFound();
+            }
+            return View(player);
+        }
+
+        // POST: Player/Edit/5
+        // Para proteger-se contra ataques de excesso de postagem, ative as propriedades específicas às quais deseja se associar. 
+        // Para obter mais detalhes, confira https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Nome,Nivel,Sexo")] Player player)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(player).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(player);
+        }
+
+        // GET: Player/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Player player = db.Player.Find(id);
+            if (player == null)
+            {
+                return HttpNotFound();
+            }
+            return View(player);
+        }
+
+        // POST: Player/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Player player = db.Player.Find(id);
+            db.Player.Remove(player);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ WebIsToMeMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebIsToMeMVC.MvcApplication" Language="C#" %>
